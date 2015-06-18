fd46d00a1f6ffaa53117aca1e439cc0d9bfde55b
Kevin Dreßler <kvndrsslr@gmail.com>
2015-06-18 11:16:57 +0200

    testing automatic rollout

--- back/resources/manual/getIndex.js
+++ back/resources/manual/getIndex.js
@@ -1,5 +1,6 @@
 exports.getIndex = function (corporaSchemes, getWordlistsDwds) {
     //getWordlistsDwds().then(console.log.bind(null, "DWDS Abfrage fertig!"));
+    //test
     var noSuffixSchemes = [];
     corporaSchemes.forEach(function (scheme) {
         if (!/_[0-9]{3}K$/.test(scheme)) {
