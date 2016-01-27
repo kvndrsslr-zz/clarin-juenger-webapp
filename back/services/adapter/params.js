28b43a2f4c70cd4d521d1cf828acd22543bf7148
Kevin Dreßler <kvndrsslr@gmail.com>
2016-01-27 17:53:49 +0100

    Attempt to fix large file problem

--- back/services/adapter/params.js
+++ back/services/adapter/params.js
@@ -1,11 +1,17 @@
 var _ = require('lodash');
 var Promise = require('bluebird');
-var bodyParser = Promise.promisify(require('body-parser').json());
+var bodyParser = Promise.promisify(require('body-parser').json({
+    'limit' : '50mb'
+}));
 
 exports.params = function (req, res) {
     // Combines the three sources of parameters into one, and return them in one object
     // Similar to how Rails and various other frameworks do it
     return bodyParser(req, res).then(function () {
         return _.merge({}, req.query, req.params, req.body, {'req' : req});
+    }).fail(function (err) {
+        console.log("file too large!");
+        console.log(err);
+        return {};
     });
 };
